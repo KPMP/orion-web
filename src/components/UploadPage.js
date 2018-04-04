b0b751481533c83253bb490e072a544b7b7185e5
Becky Reamy <rlreamy@med.umich.edu>
2018-04-04 16:01:02 -0400

    KPMP-22: Initial layout

---
+++ src/components/UploadPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class UploadPage extends Component {
+	
+	render() {
+		return (
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-sm-12">
+						<nav class="navbar navbar-light bg-primary">
+							<a class="navbar-brand" href="#">KPMP Data Lake Upload Tool</a>
+						</nav>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-8"></div>
+					<div className="col-sm-4">status</div>
+				</div>
+			HI</div>);
+	}
+}
+
+export default UploadPage;
