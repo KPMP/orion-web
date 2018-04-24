2bb94c82aa61e0cf19180ec057273065087025d9
rlreamy <34109594+rlreamy@users.noreply.github.com>
2018-04-24 15:43:02 -0400

    Changed default state to 0

--- src/actions/UploadForm/currentTabReducer.js
+++ src/actions/UploadForm/currentTabReducer.js
@@ -1,6 +1,6 @@
 import actionNames from '../actionNames';
 
-export const currentTab = (state = [], action) => {
+export const currentTab = (state = 0, action) => {
     switch(action.type) {
         case actionNames.CHANGE_UPLOAD_TAB:
             return action.payload;
