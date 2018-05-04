bf5db8f17c8e7abebb9335ff934309a5e7ca844f
Brian Royer <royerbr@med.umich.edu>
2018-05-04 13:24:39 -0400

    KPMP-58: Forgot to stop exporting store. Fixed.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const cacheStore = window.sessionStorage.getItem("redux-store");
 const initialState = cacheStore ?
     JSON.parse(cacheStore) :
     loadedState;
-export const store = applyMiddleware(thunk)(createStore)(rootReducer, initialState);
+const store = applyMiddleware(thunk)(createStore)(rootReducer, initialState);
 const saveState = () => {
   window.sessionStorage.setItem("redux-store", JSON.stringify(store.getState()));
 };
