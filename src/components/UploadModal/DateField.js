0995372c28661c56df571695a8b4af1dc6ae38c1
Zach Wright <zwright@umich.edu>
2018-05-21 20:09:21 -0400

    KPMP 58: reformatting and validation

---
+++ src/components/UploadModal/DateField.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {  ControlLabel } from 'react-bootstrap';
+import { detect } from 'detect-browser';
+import ReduxDatePicker from './ReduxDatePicker';
+
+class DateField extends Component {
+    render() {
+        let { label } = this.props;
+        let { name, type, onBlur, onChange, onFocus, value} = this.props.input;
+        let { error, touched, warning } = this.props.meta;
+        let classes = 'form-control';
+        if (touched && error) {
+            classes += ' fieldInError';
+        }
+        let browser = detect();
+        let browserName = (browser !== null) ? browser.name : "unknown";
+
+        return (
+            <div>
+                <ControlLabel>{label} &nbsp;
+					{touched &&
+                    ((error && <span className="formError ">{error}</span>) ||
+                    (warning && <span>{warning}</span>))}
+                </ControlLabel>
+                <div>
+                { (browserName === "ie") ? <ReduxDatePicker {...this.props} />
+                    : <input name={name} type="date" className={classes} onBlur={onBlur}
+                        onChange={onChange} onFocus={onFocus}/>
+                }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DateField;
