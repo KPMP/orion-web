aafbbe628624f138bce1561f074887b9b4f58d0e
Becky Reamy <rlreamy@umich.edu>
2018-07-23 15:45:15 -0400

    KPMP-148 (cgates/rlreamy): Adjusted display format of upoload package info date and time.

--- src/components/v2/Packages/PackagePanel.js
+++ src/components/v2/Packages/PackagePanel.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Panel, Col, Row, Button } from 'react-bootstrap';
+import { getLocalDateString, getLocalTimeString } from '../../../helpers/timezoneUtil';
 
 class PackagePanel extends Component {
 
     render() {
-        return (
+        var submittedDate = getLocalDateString(this.props.uploadPackage.submitted);
+        var submittedTime = getLocalTimeString(this.props.uploadPackage.submitted);
+    		return (
             <Panel className="pkg-panel">
                 <Panel.Body>
                     <Row>
@@ -15,7 +18,7 @@ class PackagePanel extends Component {
                         <Col md={4} className="pkg-panel-info">
                             <div><b>{this.props.uploadPackage.packageId}</b></div>
                             <div><a>{this.props.uploadPackage.packageType}</a></div>
-                            <div>Submitted <b>{this.props.uploadPackage.submitted}</b> at hh:mm by <a>{this.props.uploadPackage.submitter}, {this.props.uploadPackage.institution}</a></div>
+                            <div>Submitted <b>{submittedDate}</b> at {submittedTime} by <a>{this.props.uploadPackage.submitter}, {this.props.uploadPackage.institution}</a></div>
                         </Col>
                         <Col md={2} mdOffset={5} className="pkg-panel-right">
                             <div><a>nn attachments</a></div>
--- src/components/v2/Packages/packagePanelReducer.js
+++ src/components/v2/Packages/packagePanelReducer.js
@@ -4,7 +4,6 @@ export const packages = (state = [], action) => {
 	let newState = {...state}; 
 	switch(action.type) {
 		case actionNames.SET_PACKAGES:
-			console.log(action.payload);
 			newState = action.payload;
 			return newState;
 		default:
