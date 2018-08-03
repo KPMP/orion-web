5560aa60618fb63bf117dd512b60cd7115d3a7d2
Becky Reamy <rlreamy@umich.edu>
2018-08-03 13:11:30 -0400

    KPMP-215: Change the columns shown

--- src/components/UploadedFilesTable.js
+++ src/components/UploadedFilesTable.js
@@ -10,12 +10,11 @@ class UploadedFilesTable extends Component {
 		this._columns = [
 		      { key: 'researcher', name: 'Name', resizable: true, sortable: true, formatter: DefaultFormatter },
 		      { key: 'institution', name: 'Site Name', resizable: true, sortable: true, formatter: DefaultFormatter },
+		      { key: 'packageId', name: 'Package ID', resizable: true, sortable: true, formatter: DefaultFormatter },
 		      { key: 'packageType', name: 'Package Type', resizable: true, sortable: true, formatter: DefaultFormatter },
-		      { key: 'filename', name: 'File Name', resizable: true, sortable: true, formatter: DefaultFormatter },
 		      { key: 'subjectId', name:'Subject #', resizable: true, sortable: true, formatter: DefaultFormatter },
-		      { key: 'experimentId', name:'Experiment #', resizable: true, sortable: true, formatter: DefaultFormatter },
-		      { key: 'experimentDate', name: 'Experiment Date', formatter: DateFormatter, resizable: true, sortable: true},
-			  { key: 'createdAt', name: 'Upload Date', formatter: DateTimeFormatter, resizable: true, sortable: true} ];
+			  { key: 'createdAt', name: 'Upload Date', formatter: DateTimeFormatter, resizable: true, sortable: true},
+			  { key: 'download', name: 'Download', resizable: true, sortable: false, formatter: DefaultFormatter }];
 
 		let rows = props.uploadedFiles;
 		this.state = { rows };
