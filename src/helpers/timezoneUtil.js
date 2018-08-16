8d26a458e6a08fccfcdb62b59d91a869aef7f959
Zach Wright <zwright@umich.edu>
2018-08-16 13:59:25 -0400

    KPMP-227: IE-friendly date

--- src/helpers/timezoneUtil.js
+++ src/helpers/timezoneUtil.js
@@ -1,9 +1,15 @@
 import dateFormat from 'dateformat';
 
 export const getLocalDateString = (timestampString) => {
-    return dateFormat(new Date(timestampString), 'yyyy-mm-dd', false);
+    let ieFriendlyDate = getIEFriendlyDate(timestampString);
+    return dateFormat(new Date(ieFriendlyDate), 'yyyy-mm-dd', false);
 }
 
 export const getLocalTimeString = (timestampString) => {
-    return dateFormat(new Date(timestampString), 'h:MM TT', false);
+    let ieFriendlyDate = getIEFriendlyDate(timestampString);
+    return dateFormat(new Date(ieFriendlyDate), 'h:MM TT', false);
+}
+
+export const getIEFriendlyDate = (timestampString) => {
+    return timestampString.replace("+0000", "Z");
 }
