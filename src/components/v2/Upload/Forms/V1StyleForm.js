e8d047134eb0dd40918ced1dcfdb8205d74e204a
Becky Reamy <rlreamy@umich.edu>
2018-09-05 15:17:54 -0400

    KPMP-252: Extract protocols and institutions to external json files

--- src/components/v2/Upload/Forms/V1StyleForm.js
+++ src/components/v2/Upload/Forms/V1StyleForm.js
@@ -5,6 +5,8 @@ import TextField from './TextField';
 import DateField from './DateField';
 import SelectBox from './SelectBox';
 import moment from 'moment';
+import protocolList from './protocols';
+import institutionList from './institutions';
 
 const validate = (values) => {
 	const errors = {};
@@ -35,28 +37,7 @@ const validate = (values) => {
 class V1StyleForm extends Component {
 	
 	render() {
-		let institutionOptions = [ {'value': 'Broad (Michigan/Broad/Princeton TIS)', 'label': 'Broad (Michigan/Broad/Princeton TIS)'},
-			{'value': 'EMBL (UTHSA/EMBL/PNNL/UCSD TIS)', 'label': 'EMBL (UTHSA/EMBL/PNNL/UCSD TIS)'},
-			{'value': 'Indiana (IU/OSU TIS)', 'label': 'Indiana (IU/OSU TIS)'},
-			{'value': 'Michigan (Michigan/Broad/Princeton TIS)', 'label': 'Michigan (Michigan/Broad/Princeton TIS)'},
-			{'value': 'OSU (IU/OSU TIS)', 'label': 'OSU (IU/OSU TIS)'},
-			{'value': 'PNNL (UTHSA/EMBL/PNNL/UCSD TIS)', 'label': 'PNNL (UTHSA/EMBL/PNNL/UCSD TIS)'},
-			{'value': 'Princeton (Michigan/Broad/Princeton TIS)', 'label': 'Princeton (Michigan/Broad/Princeton TIS)'},
-			{'value': 'Stanford (UCSF/Stanford TIS)', 'label': 'Stanford (UCSF/Stanford TIS)'},
-			{'value': 'UCSD (UCSD/WashU TIS)', 'label': 'UCSD (UCSD/WashU TIS)'},
-			{'value': 'UCSD (UTHSA/EMBL/PNNL/UCSD TIS)', 'label': 'UCSD (UTHSA/EMBL/PNNL/UCSD TIS)'},
-			{'value': 'UCSF (UCSF/Stanford TIS)', 'label': 'UCSF (UCSF/Stanford TIS)'},
-			{'value': 'UTHSA (UTHSA/EMBL/PNNL/UCSD TIS)', 'label': 'UTHSA (UTHSA/EMBL/PNNL/UCSD TIS)'},
-			{'value': 'WashU (UCSD/WashU TIS)', 'label': 'WashU (UCSD/WashU TIS)'},
-			];
 		
-		let protocols = [{'value': 'Pilot 1', 'label': 'Pilot 1'},
-			{'value': 'Pilot 2', 'label': 'Pilot 2'},
-			{'value': 'Pilot 3', 'label': 'Pilot 3'},
-			{'value': 'Protocol v1: Pathology MOP v1 ', 'label': 'Protocol v1: Pathology MOP v1 '},
-			{'value': 'Protocol v1: TIS MOP v1 ', 'label': 'Protocol v1: TIS MOP v1 '},
-			{'value': 'Other', 'label': 'Other'},
-			];
 		return(
 			<Form name="uploadPackageInfoForm" id="uploadPackageInfoForm">
             <div id="uploadInfo">
@@ -67,10 +48,10 @@ class V1StyleForm extends Component {
                     <Field name="lastName" component={TextField} label="Last Name" type="text" />
                 </div>
                 <div className="form-group">
-                    <Field name="institutionName" label="Site Name" className="form-control" component={SelectBox} options={institutionOptions} />
+                    <Field name="institutionName" label="Site Name" className="form-control" component={SelectBox} options={institutionList.institutions} />
                 </div>
                 <div className="form-group">
-                		<Field name="protocol" className="form-control" label="Associated Protocol" component={SelectBox} options={protocols} />
+                		<Field name="protocol" className="form-control" label="Associated Protocol" component={SelectBox} options={protocolList.protocols} />
                 	</div>
                 <div className="form-group">
                     <ControlLabel>Subject # (optional)</ControlLabel>
