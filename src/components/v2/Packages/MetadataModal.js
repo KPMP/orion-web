3478f0e9319718f962b60dacd69e8de849d5f820
Zach Wright <zwright@umich.edu>
2018-09-11 14:15:06 -0400

    KPMP_150: Metadata on modal

--- src/components/v2/Packages/MetadataModal.js
+++ src/components/v2/Packages/MetadataModal.js
@@ -13,7 +13,10 @@ class MetadataModal extends Component {
                             <Modal.Title>Provided Metadata</Modal.Title>
                         </Modal.Header>
                         <Modal.Body className="metadataModalBody">
-
+                            <div>Package type: {this.props.uploadPackage.packageType}</div>
+                            <div>Subject ID: {this.props.uploadPackage.subjectId}</div>
+                            Experiment number: AAAA-1234-YY-ZZZZZ
+                            Experiment date: 2018-05-31
                         </Modal.Body>
                     </Modal>
                 </div>
