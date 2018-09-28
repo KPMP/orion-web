d805bb8a7b3a7a6f2f13f8a4578f649f6dad848c
Becky Reamy <rlreamy@umich.edu>
2018-09-28 10:17:11 -0400

    KPMP-200: [skip ci] add some logging for tracing

--- src/actions/v2/userActions.js
+++ src/actions/v2/userActions.js
@@ -2,15 +2,16 @@ import Api from '../../helpers/Api';
 const api = Api.getInstance();
 
 export const getUserInformation = () => {
+	console.log("outside return in getUserInformation");
 	return (dispatch) => {
 		console.log("in get user information ");
 		api.get('/api/v1/attributes')
-		.then(res => {
-			console.log(res)
-		})
-		.catch(err => {
-            alert("Unable to retrieve user information");
-            console.error(err);
-        });
+			.then(res => {
+				console.log(res)
+			})
+			.catch(err => {
+	            alert("Unable to retrieve user information");
+	            console.error(err);
+	        });
 	};
 } 
