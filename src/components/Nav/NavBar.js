3b097576c491a0b0823fb378b44b7ab07d166746
Becky Reamy <rlreamy@umich.edu>
2018-10-09 10:59:56 -0400

    KPMP-266: Change the logout link to redirect back to our login

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -33,7 +33,7 @@ class NavBar extends Component {
                         {this.props.displayName}
                     </Navbar.Text>
                     <Navbar.Text>
-                        <a href="/Shibboleth.sso/Logout?return=/">Sign out</a>
+                        <a href="/Shibboleth.sso/Logout?return=/Shibboleth.sso/Login">Sign out</a>
                     </Navbar.Text>
                 </Nav>
             </Navbar>
