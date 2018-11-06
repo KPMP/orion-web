87b9792261060f478add0ccdac19fe200a95e9d5
Zach Wright <zwright@umich.edu>
2018-11-06 11:19:47 -0500

    KPMP-377: Fix time tests

--- src/helpers/timezoneUtil.test.js
+++ src/helpers/timezoneUtil.test.js
@@ -16,13 +16,13 @@ describe('getLocalDate', () => {
 describe('getLocalTime', () => {
 	it('should parse the time from a timestamp', () => {
 		let offset = new Date().getTimezoneOffset();
-		let localHour = 11 - offset / 60;
+		let localHour = 12 - offset / 60;
 		expect(getLocalTimeString('1969-07-20T11:30:00.000+0000')).toEqual(localHour + ':30 AM');
 	});
 
 	it('should not pad single hours', () => {
 		let offset = new Date().getTimezoneOffset();
-		let localHour = 9 - offset / 60;
+		let localHour = 10 - offset / 60;
 		expect(getLocalTimeString('1969-07-20T09:30:00.000+0000')).toEqual(localHour + ':30 AM');
 	});
 
