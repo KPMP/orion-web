1cf1ba3648c46ec8cf4a260c473ef941c734d012
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2019-03-04 12:27:51 -0500

    KPMP-742 functionally added new isQuerying state to redux, actions, reducers

--- src/components/Packages/PackageList.js
+++ src/components/Packages/PackageList.js
@@ -4,24 +4,39 @@ import PackagePanel from './PackagePanel';
 class PackageList extends Component {
 
     componentDidMount() {
-        this.props.getPackages();
+        //this.props.getPackages();
+        console.log('!!! DEBUG!  Disabled package loading');
     }
 
     render() {
-        const panels = this.props.packages.filtered.map((uploadPackage, index) => {
-            return <PackagePanel index={index} uploadPackage={uploadPackage}/>;
-        });
+        let isQuerying = this.props.packages.isQuerying === null || this.props.packages.isQuerying;
+        let panels = [];
+
+        if(!isQuerying) {
+            panels = this.props.packages.filtered.map((uploadPackage, index) => {
+                return <PackagePanel index={index} uploadPackage={uploadPackage}/>;
+            });
+        }
+
         return (
         	<div>
-	    		{panels.length > 0 ?
-	    			<div id="pkg-list">
-	            		{panels}
-	           		</div>
-	            : 
-	            	<div className="noResults alert alert-info">
-	            		No packages returned for the selected criteria.
-	            	</div>
-	    		}
+                {isQuerying ?
+
+                    <div id="pkg-querying">
+                        Loading packages...
+                    </div>
+
+                    :
+
+                    panels.length > 0 ?
+                        <div id="pkg-list">
+                            {panels}
+                        </div>
+                        :
+                        <div className="noResults alert alert-info">
+                            No packages returned for the selected criteria.
+                        </div>
+                    }
     		</div>
         );
     }
