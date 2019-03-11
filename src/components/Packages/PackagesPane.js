dd99ccfdac8e2019ef5d8aac4ebfb2b8ead7dd43
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2019-03-11 08:01:20 -0400

    KPMP-582 IE detection support for resizing/collision issue

--- src/components/Packages/PackagesPane.js
+++ src/components/Packages/PackagesPane.js
@@ -5,6 +5,7 @@ import { Button, Row, Col } from 'reactstrap';
 import institutions from '../institutions';
 import packageTypes from '../packageTypes';
 import * as filterActions from '../../actions/filterActions';
+import detectIE from '../../helpers/detectIE';
 
 class PackagesPane extends Component {
 	
@@ -20,7 +21,7 @@ class PackagesPane extends Component {
     render() {
     	let userOptions = this.usersToOptions(this.props.users);
         return (
-    		<article id="packages-pane" className="container">
+    		<article id="packages-pane" className={"container" + (detectIE() ? " ie" : "")}>
     			<header id="packages-filter-controls" className="container-fluid fixed-top-subnav pt-3">
 					<Row noGutters>
 						<Col xs={12} md={"auto"} className="mx-sm-auto ml-md-0 mr-md-2">
