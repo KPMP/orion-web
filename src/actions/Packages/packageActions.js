be5a5de216fb9a495366948f7d30b53e31b8da37
Becky Reamy <rlreamy@umich.edu>
2019-03-13 11:08:32 -0400

    KPMP-721: Minor cleanup

--- src/actions/Packages/packageActions.js
+++ src/actions/Packages/packageActions.js
@@ -66,6 +66,7 @@ export const uploadPackage = (packageInfo, uploader) => {
 		lastName: packageInfo.submitterLastName,
 		email: packageInfo.submitterEmail
 	};
+	
 	let activeFiles = uploader.methods.getUploads({
 		status: [ qq.status.SUBMITTED, qq.status.PAUSED ]});
 	packageInfo.files = activeFiles.map((file) => {
