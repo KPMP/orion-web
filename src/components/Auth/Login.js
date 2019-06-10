adaa886512e4fcc507d683c3fe80c298a7743d56
Zach Wright <zwright@umich.edu>
2019-06-10 14:44:36 -0400

    KPMP-696: Unused styff

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            authPending: 1
-        }
-    }
-
+    
     componentWillMount() {
     	console.log("in login")
         window.location.href = this.props.loginURL;
