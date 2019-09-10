1a1ea709b0c247eb8f3114723a8aa4a37500afff
Zach Wright <zwright@umich.edu>
2019-09-10 14:17:49 -0400

    KPMP-1246: Get rid of warnings

--- src/components/Packages/LargeFileModal.js
+++ src/components/Packages/LargeFileModal.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import {Modal, ModalBody, ModalHeader, ModalFooter, Col} from 'reactstrap';
+import {Modal, ModalBody, ModalHeader, ModalFooter} from 'reactstrap';
 import PropTypes from 'prop-types';
-import {Button, Tree} from "antd";
+import {Button} from "antd";
 
 class LargeFileModal extends Component {
-
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
@@ -19,7 +15,7 @@ class LargeFileModal extends Component {
                     <ModalBody className="largeFileModalBody">
                         <p>This method requires you to do the following in order to upload your file(s) into the data lake:</p>
                         <ol>
-                            <li>Click this link to <a href="#">open the destination folder</a> on Google Drive.</li>
+                            <li>Click this link to <a href="https://google.com">open the destination folder</a> on Google Drive.</li>
                             <li>Upload your files to the Google Drive folder.</li>
                             <li>Email <a href="mailto:datauploadersupport@kpmp.org">datauploadersupport@kpmp.org</a> to let us know you have finished uploading your file(s).</li>
                         </ol>
