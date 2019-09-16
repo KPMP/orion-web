5255e55e475866eaa96a9fd7af3935df03a65bd1
Zach Wright <zwright@umich.edu>
2019-09-16 14:24:58 -0400

    KPMP-1249: Get that link right

--- src/components/Packages/LargeFileModal.js
+++ src/components/Packages/LargeFileModal.js
@@ -15,7 +15,7 @@ class LargeFileModal extends Component {
                     <ModalBody className="largeFileModalBody">
                         <p>This method requires you to do the following in order to upload your file(s) into the data lake:</p>
                         <ol>
-                            <li>Click this link to <a href={this.props.link}>open the destination folder</a> on Google Drive.</li>
+                            <li>Click this link to <a href={"https://drive.google.com/drive/folders/" + this.props.link} target="_blank">open the destination folder</a> on Google Drive.</li>
                             <li>Upload your files to the Google Drive folder.</li>
                             <li>Email <a target="_blank" rel="noopener noreferrer" href="mailto:datauploadersupport@kpmp.org">datauploadersupport@kpmp.org</a> to let us know you have finished uploading your file(s).</li>
                         </ol>
