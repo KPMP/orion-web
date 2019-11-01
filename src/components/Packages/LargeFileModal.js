a8817760b608f3dc79bd333672d2cfd2461bcb84
Zach Wright <zwright@umich.edu>
2019-11-01 16:11:56 -0400

    KPMP-Globus_API: fix link

--- src/components/Packages/LargeFileModal.js
+++ src/components/Packages/LargeFileModal.js
@@ -15,7 +15,7 @@ class LargeFileModal extends Component {
                     <ModalBody className="largeFileModalBody">
                         <p>This method requires you to do the following in order to upload your file(s) into the data lake:</p>
                         <ol>
-                            <li>Click this link to <a rel="noopener noreferrer" href={"https://drive.google.com/drive/folders/" + this.props.link} target="_blank">open the destination folder</a> on Google Drive.</li>
+                            <li>Click this link to <a rel="noopener noreferrer" href={this.props.link} target="_blank">open the destination folder</a> on Google Drive.</li>
                             <li>Upload your files to the Google Drive folder. <br/>(NOTE: You may need to sign in with a Google account)</li>
                             <li>Email <a target="_blank" rel="noopener noreferrer" href="mailto:datalakeuploadersupport@kpmp.org">datalakeuploadersupport@kpmp.org</a> to let us know you have finished uploading your file(s).</li>
                         </ol>
