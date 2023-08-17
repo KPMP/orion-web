cb34a10a2930e4588896fc050a80d20146886d46
dert1129 <nathancreger1111@gmail.com>
2023-08-17 12:02:34 -0400

    fix ref

--- src/components/Upload/Forms/FormComponents/SubmitterInformation.js
+++ src/components/Upload/Forms/FormComponents/SubmitterInformation.js
@@ -16,7 +16,7 @@ class SubmitterInformation extends Component {
             submitterEmailDisabled: this.isFieldDisabled(EMAIL, props),
             userInfoPopulated: this.isUserInfoPopulated(props)
 		};
-    this.formRef = React.useForm(null);
+    this.formRef = React.createRef(null);
 	}
 
 	isFieldDisabled(fieldName, props) {
