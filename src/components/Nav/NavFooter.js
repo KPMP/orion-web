3c126e7f4d24c187f2a713e724f001dee1ece604
Zach Wright <zwright@umich.edu>
2024-04-17 10:06:21 -0400

    IP-406: Site link

--- src/components/Nav/NavFooter.js
+++ src/components/Nav/NavFooter.js
@@ -6,7 +6,7 @@ class NavFooter extends Component {
             <div id="footer" className="fixed-bottom px-1 py-1">
                 <div>
                 <a className="text-light small"
-                   href="https://kpmp.org" target="_blank"  rel="noopener noreferrer"
+                   href="https://curegn.org" target="_blank"  rel="noopener noreferrer"
                 >&copy; Cure Glomerulonephropathy Network</a>
                 </div>
                 <div>
