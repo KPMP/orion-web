5a9fc1f94627579e26ed21daacddf030fe8d52a9
Nathan Creger <nathancreger1111@gmail.com>
2024-04-19 11:53:18 -0400

    dispatch setStudy and setBiopsyIds

--- src/actions/Upload/uploadActions.js
+++ src/actions/Upload/uploadActions.js
@@ -1,7 +1,7 @@
 import Api from '../../helpers/Api';
 import actionNames from '../actionNames';
 import { sendMessageToBackend } from '../Error/errorActions';
-import { setPackageTypesFromDTD, setSiteNamesFromDTD } from '../filterActions.js';
+import { setPackageTypesFromDTD, setSiteNamesFromDTD, setBiopsyIDsFromDTD, setStudyNamesFromDTD } from '../filterActions.js';
 
 const api = Api.getInstance();
 
@@ -19,6 +19,8 @@ export const getFormDTD = () => {
 				dispatch(setFormDTD(res.data));
 				dispatch(setPackageTypesFromDTD(res.data));
 				dispatch(setSiteNamesFromDTD(res.data));
+                dispatch(setBiopsyIDsFromDTD(res.data));
+                dispatch(setStudyNamesFromDTD(res.data));
 			})
 			.catch((err) => {
 				dispatch(sendMessageToBackend(err));
