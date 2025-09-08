4e132b477727342f62671228ecd3c9557e840802
dert1129 <nathancreger1111@gmail.com>
2025-09-08 09:21:54 -0400

    use addFilter method

--- src/components/Packages/FilterControl.js
+++ src/components/Packages/FilterControl.js
@@ -11,7 +11,7 @@ class FilterControl extends Component {
 	
 	componentDidMount() {
         if (this.props.defaultFilter){
-            this.setState({selectedOption: this.props.defaultFilter});
+            this.addFilter(this.props.defaultFilter);
         }
 		this.props.removeFilter(this.props.type, this.state.selectedOption);
 	}
