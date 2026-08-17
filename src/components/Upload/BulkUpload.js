7283927917ba8c20084adf235dab08c1c97cdc5a
dert1129 <nathancreger1111@gmail.com>
2026-08-17 11:19:12 -0400

    use default export

---
+++ src/components/Upload/BulkUpload.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class BulkUpload extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the Bulk Upload page</h1>
+            </div>
+        );
+    }
+}
+export default BulkUpload;
